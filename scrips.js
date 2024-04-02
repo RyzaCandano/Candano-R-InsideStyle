f37783ba76e7bc4acd1cf8914391c909c9f697d7
denver154 <magildaceria@gmail.com>
2024-04-02 15:19:22 +0800

    first commit home page

---
+++ scrips.js
@@ -0,0 +1,8 @@
+const headerNav = document.querySelector (".header_nav");
+const toggleMenu = document.querySelector (".toggle_menu");
+
+toggleMenu.addEventListener("click", () => {
+    headerNav.classList.toggle("open");
+    toggleMenu.classList.toggle("open");
+
+})
